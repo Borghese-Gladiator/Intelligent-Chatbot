501fc05fecaa28995662378e935d4eec3fa886b9
Timothy Shee <timothy_shee@hotmail.com>
2020-05-14 01:48:34 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <Router>
       <div className="App">
         <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
