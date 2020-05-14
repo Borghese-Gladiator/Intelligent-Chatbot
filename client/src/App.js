799600000926996eea5a09df82c0b7eee6d2a10c
Timothy Shee <timothy_shee@hotmail.com>
2020-05-13 23:21:11 -0400

    Prepare for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      console.log(data)
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +27,12 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
