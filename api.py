3d62086befb5fe977f56d968cda76aeeeb7a7b8e
Timothy Shee <timothy_shee@hotmail.com>
2020-05-14 01:22:21 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 
-app = Flask(__name__, static_folder='client/build')
+app = Flask(__name__, static_folder='./client/build/static', static_url_path='/')
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
@@ -10,11 +14,9 @@ def serve(path):
     if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
+        # return send_from_directory(app.static_folder, 'index.html')
         return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/api/time')
-def get_current_time():
-    return {'time': time.time()}
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
+	
